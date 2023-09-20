1b39a7c9f1108d57a974045a5ae6c82878527ae7
Marshal <mpetry2@hotmail.com>
2023-09-20 13:42:48 -0300

    fix fetTodosGroupedByColumn

--- src/lib/getTodosGroupedByColumn.ts
+++ src/lib/getTodosGroupedByColumn.ts
@@ -3,8 +3,8 @@ import { databases } from "@/appwrite"
 
 export const getTodosGroupedByColumns = async () => {
     const data = await databases.listDocuments(
-        process.env.NEXT_PUBLIC_DATABASE_ID,
-        process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID
+        process.env.NEXT_PUBLIC_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
     );
 
     const todos = data.documents;
